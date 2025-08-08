ca24e30eb0c6c57597395a0d1d42412d3d85fedd
ADMIN <ADMIN@RAGAVIarunagiri>
2025-08-08 20:14:41 +0530

    SpringCore2

---
+++ springcore2/src/main/java/com/tnsif/springcore2/App.java
@@ -0,0 +1,18 @@
+package com.tnsif.springcore2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring_config.xml");
+    	College college = context.getBean("college",College.class);
+    	college.showCollegeDetails();
+    }
+}
---
+++ springcore2/src/main/java/com/tnsif/springcore2/Student.java
@@ -0,0 +1,23 @@
+package com.tnsif.springcore2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Student {
+	private String name ="Rags";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void showStudent() {
+		System.out.println("Student Name:" + name);
+	}
+		
+	}
+
+
